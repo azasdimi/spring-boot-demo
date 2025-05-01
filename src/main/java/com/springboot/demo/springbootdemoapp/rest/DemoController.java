27cbac147080d18e04013e91220a9447e8fd5991
Dimitris Azas <dimitrisazas@macbookpro.ams.nl.ibm.com>
2025-05-01 15:29:10 +0200

    Override Qualifier annotator by assigning a Primary Component

--- src/main/java/com/springboot/demo/springbootdemoapp/rest/DemoController.java
+++ src/main/java/com/springboot/demo/springbootdemoapp/rest/DemoController.java
@@ -14,7 +14,7 @@ public class DemoController {
 
     //define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("tennisCoach") Coach coach) {
+    public DemoController( Coach coach) {
         myCoach = coach;
     }
 
